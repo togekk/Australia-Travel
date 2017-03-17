a47174b406e1d64b106d3578a32b84dcfcfdf2dc
togekk <togekk@gmail.com>
2017-03-17 18:17:53 +0800

    Using Web Animaiton API in scrolling animations

--- src/compatibility.js
+++ src/compatibility.js
@@ -1,13 +1,17 @@
-function check() {
+function web_animation_api_check() {
     "use strict";
 
     try {
-      eval("const onload = () => {}");
+      eval("document.querySelector('html').animate({ opacity: [1, 0] }, 1)");
     }
     catch (e) { return false; }
     return true;
 }
 
-if (!check()) {
-    window.location.href = "src/redirect.html";
+let waapi = true;
+
+if (!web_animation_api_check()) {
+    // window.location.href = "src/redirect.html";
+    console.log('Web Animation API not supported. Using GSAP');
+    waapi = false;
 }
